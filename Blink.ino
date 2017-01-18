69391e9c5ff10c6290d8e545082192f78055d47c
Dearth96 <rule96moni@gmail.com>
2017-01-18 16:34:56 +0530

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,12 @@
+const int LED = 13;// Pin Number//
+void setup(){
+  pinMode(LED, OUTPUT);   // writing into the LED//
+}
+
+void loop(){
+  digitalWrite(LED, HIGH);//Setting the pin LED to HIGH//
+  delay(1000);
+  digitalWrite(LED, LOW);//Setting the pin LED to LOW//
+  delay(1000);
+}
+
