e7235107891c89d8d05f475a5322b0fe5befc64d
Dearth96 <rule96moni@gmail.com>
2017-01-18 19:34:34 +0530

    Add files via upload

---
+++ Light_Sensing.ino
@@ -0,0 +1,18 @@
+const int LED = 13;
+const int LS = 7;
+int val = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED, OUTPUT);
+pinMode(LS, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val = digitalRead(LS);
+if(val ==HIGH){
+  digitalWrite(LED, HIGH);
+}else{
+  digitalWrite(LED, LOW);
+}
+}
