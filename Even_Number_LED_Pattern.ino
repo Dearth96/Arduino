a35fef2c0b869a7633ea567b8d461a0e3aa073f8
Dearth96 <rule96moni@gmail.com>
2017-01-18 16:22:31 +0530

    Update Even_Number_LED_Pattern.ino

--- Even_Number_LED_Pattern.ino
+++ Even_Number_LED_Pattern.ino
@@ -1,13 +1,13 @@
-const int a = 3;
-const int k[a] = {11,12,13};
-void setup(){
+const int a = 3; // a will be the value of the number of LED's that will be used//
+const int k[a] = {11,12,13};  // The analog pins that will connected to the LED's//
+void setup(){          
   for(int i = 0; i<a;i++){
-  pinMode(k[i], OUTPUT);
-  }
+  pinMode(k[i], OUTPUT);     // Setting the Pin mode to Write//
+   }
 }
 void loop(){
   for(int i =0;i<a;i++){
-    if(i % 2 == 0){
+    if(i % 2 == 0){   // When i divide with 2 gives a remainder.If there is a remainder then it won't light up// 
     digitalWrite(k[i], HIGH);
     delay(1000);
    }
